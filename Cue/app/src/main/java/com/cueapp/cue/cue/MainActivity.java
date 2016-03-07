12107026950707d8a30e2f4fed073469cb37e631
Ryan Carrell <Ryan Carrell>
2016-03-07 15:39:38 -0500

    linked activities

--- Cue/app/src/main/java/com/cueapp/cue/cue/MainActivity.java
+++ Cue/app/src/main/java/com/cueapp/cue/cue/MainActivity.java
@@ -30,13 +30,6 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
 //        ListView lst = new ListView();
 //        String[] arr = {"Item 1","Item 2"};
