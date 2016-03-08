d8c19662b6ce6ce37b8ceff19a8ad91648482612
Ryan Carrell <Ryan Carrell>
2016-03-08 16:21:00 -0500

    saving reminders to database

--- Cue/app/src/main/java/com/cueapp/cue/cue/CreateNotificationActivity.java
+++ Cue/app/src/main/java/com/cueapp/cue/cue/CreateNotificationActivity.java
@@ -1,17 +1,28 @@
 package com.cueapp.cue.cue;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.Firebase;
+import com.facebook.FacebookSdk;
+import android.content.Intent;
 
 public class CreateNotificationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_create_notification);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -23,16 +34,15 @@ public class CreateNotificationActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-        
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void submitReminder(View view) {
+
+        Firebase ref = new Firebase("https://cue-app.firebaseio.com/");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
