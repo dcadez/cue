a853eb8aa921e5ceeb98ed7ef6a33fbad23ad298
dcadez <dominic.cadez@gmail.com>
2016-03-08 15:21:39 -0500

    add initial activity with facebook login button as fragment

---
+++ Cue/app/src/main/java/com/cueapp/cue/cue/FacebookLoginActivityFragment.java
@@ -0,0 +1,22 @@
+package com.cueapp.cue.cue;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FacebookLoginActivityFragment extends Fragment {
+
+    public FacebookLoginActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_facebook_login, container, false);
+    }
+}
