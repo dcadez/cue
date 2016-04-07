bb0edc00d2de8d47c509c43010c2273493d10167
Sevag Nadjarian <sevnadj@umich.edu>
2016-04-07 16:09:54 -0400

    Finished date/time picker

--- Cue/app/src/main/java/com/cueapp/cue/cue/CreateNotificationActivity.java
+++ Cue/app/src/main/java/com/cueapp/cue/cue/CreateNotificationActivity.java
@@ -29,6 +29,9 @@ import android.widget.TimePicker;
 
 import org.json.JSONArray;
 
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -38,6 +41,22 @@ public class CreateNotificationActivity extends AppCompatActivity {
 
     private EditText editDate;
     private EditText editTime;
+    Calendar myCalendar;
+
+    private String getTime(int hr,int min) {
+        Time tme = new Time(hr,min,0);//seconds by default set to zero
+        Format formatter;
+        formatter = new SimpleDateFormat("h:mm a");
+        return formatter.format(tme);
+    }
+
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        editDate.setText(sdf.format(myCalendar.getTime()));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,31 +76,77 @@ public class CreateNotificationActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+//        DateTime inspected_at = DateTime.now();                // Typically pulled from DB.
+//
+//        int year    = inspected_at.getYear() ;
+//        int month   = inspected_at.getMonthOfYear() - 1;      // Need to subtract 1 here.
+//        int day     = inspected_at.getDayOfMonth();
+//        int hour    = inspected_at.getHourOfDay();
+//        int minutes = inspected_at.getMinuteOfHour();
+
+
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
         editDate = (EditText) findViewById(R.id.editDate);
+        editTime = (EditText) findViewById(R.id.editTime);
+
+        DateFormat dateFormat = new SimpleDateFormat("h:mm a");
+        String currentTime = dateFormat.format(new Date()).toString();
+
+        Log.e("CURRENT TIME", currentTime);
+        editTime.setText(currentTime);
+
+        myCalendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
 
         editDate.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                //To show current date in the datepicker
-                Calendar mcurrentDate = Calendar.getInstance();
-                int mYear = mcurrentDate.get(Calendar.YEAR);
-                int mMonth = mcurrentDate.get(Calendar.MONTH);
-                int mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog mDatePicker = new DatePickerDialog(CreateNotificationActivity.this, new DatePickerDialog.OnDateSetListener() {
-                    public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
-                        // TODO Auto-generated method stub
-                    /*      Your code   to get date and time    */
-                    }
-                }, mYear, mMonth, mDay);
-                mDatePicker.setTitle("Select date");
-                mDatePicker.show();
+                new DatePickerDialog(CreateNotificationActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
+//        editDate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//                //To show current date in the datepicker
+//                Calendar mcurrentDate = Calendar.getInstance();
+//                int mYear = mcurrentDate.get(Calendar.YEAR);
+//                int mMonth = mcurrentDate.get(Calendar.MONTH);
+//                int mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+//
+//                DatePickerDialog mDatePicker = new DatePickerDialog(CreateNotificationActivity.this, new DatePickerDialog.OnDateSetListener() {
+//                    public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+//                        // TODO Auto-generated method stub
+//                    /*      Your code   to get date and time    */
+//                    }
+//                }, mYear, mMonth, mDay);
+//                mDatePicker.setTitle("Select date");
+//                mDatePicker.show();
+//            }
+//        });
 
-        editTime = (EditText) findViewById(R.id.editTime);
+
+//        editTime = (EditText) findViewById(R.id.editTime);
         editTime.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -93,8 +158,18 @@ public class CreateNotificationActivity extends AppCompatActivity {
                 TimePickerDialog mTimePicker;
                 mTimePicker = new TimePickerDialog(CreateNotificationActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        editTime.setText( selectedHour + ":" + selectedMinute);
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int selectedMinute) {
+                        String AM_PM ;
+                        if(hourOfDay < 12) {
+                            AM_PM = "AM";
+                        } else {
+                            AM_PM = "PM";
+                        }
+
+                        String formatter = getTime(hourOfDay, selectedMinute);
+
+//                      editTime.setText(hourOfDay + " : " + selectedMinute + " " + AM_PM );
+                        editTime.setText(formatter);
                     }
                 }, hour, minute, false);//No 24 hour time
                 mTimePicker.setTitle("Select Time");
@@ -103,14 +178,6 @@ public class CreateNotificationActivity extends AppCompatActivity {
         });
 
     }
-
-
-//    EditText editDate;
-
-
-//    editDate = findViewById(R.id.editDate);
-//    editDate = (EditText) findViewById(R.id.editDate);
-
 
     public void submitReminder(View view) {
 
@@ -124,48 +191,3 @@ public class CreateNotificationActivity extends AppCompatActivity {
         startActivity(intent);
     }
 }
-
-
-//Calendar myCalendar = Calendar.getInstance();
-//EditText editDate = (EditText)findViewById(R.id.editDate);
-//
-//DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-//
-//    @Override
-//    public void onDateSet(DatePicker view, int year, int monthOfYear,
-//                          int dayOfMonth) {
-//        // TODO Auto-generated method stub
-//        myCalendar.set(Calendar.YEAR, year);
-//        myCalendar.set(Calendar.MONTH, monthOfYear);
-//        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-//        updateLabel();
-//    }
-//};
-//
-//
-////    mEditInit.setOnClickListener(new View.OnClickListener() {
-////        @Override
-////        public void onClick(View v) {
-////            showDialog(DATEINIT_DIALOG);
-////        }
-////
-////    });
-//
-//editDate.setOnClickListener(new View.OnClickListener() {
-//
-////        @Override
-//public void onClick(View v) {
-//        // TODO Auto-generated method stub
-//        new DatePickerDialog(CreateNotificationActivity.this, date, myCalendar
-//        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
-//        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
-//        }
-//        });
-//
-//private void updateLabel() {
-//
-//        String myFormat = "MM/dd/yy"; //In which you need put here
-//        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
-//
-//        editDate.setText(sdf.format(myCalendar.getTime()));
-//        }
