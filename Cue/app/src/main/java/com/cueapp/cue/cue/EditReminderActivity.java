31110fa5e4f61a07a2beedfe789c3770666f2853
dcadez <dominic.cadez@gmail.com>
2016-04-07 15:48:55 -0400

    add bare-bones functions to buttons, activity changes

--- Cue/app/src/main/java/com/cueapp/cue/cue/EditReminderActivity.java
+++ Cue/app/src/main/java/com/cueapp/cue/cue/EditReminderActivity.java
@@ -1,7 +1,9 @@
 package com.cueapp.cue.cue;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EditReminderActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class EditReminderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_reminder);
+    }
+
+    public void saveChanges(View view) {
+        Intent intent = new Intent(this, ViewReminderActivity.class);
+        startActivity(intent);
+    }
+
+    public void cancelChanges(View view) {
+        Intent intent = new Intent(this, ViewReminderActivity.class);
+        startActivity(intent);
     }
 }
--- Cue/app/src/main/java/com/cueapp/cue/cue/ViewReminderActivity.java
+++ Cue/app/src/main/java/com/cueapp/cue/cue/ViewReminderActivity.java
@@ -1,7 +1,9 @@
 package com.cueapp.cue.cue;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewReminderActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class ViewReminderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_reminder);
+    }
+
+    public void editReminder(View view) {
+        Intent intent = new Intent(this, EditReminderActivity.class);
+        startActivity(intent);
+    }
+
+    public void deleteReminder(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
